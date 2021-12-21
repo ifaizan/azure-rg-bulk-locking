fc06b93350931a63ab867187b2a08729fcf647c4
Faizan Younus <50990523+ifaizan@users.noreply.github.com>
2021-12-22 00:26:22 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "subscription_id" {
   type        = string
-  description = "The Subscription ID name"
-  default     = "be54c1cb-556b-4045-abe7-2c27e2223e6f"
+  description = "The Subscription ID"
+  default     = ""
 }
